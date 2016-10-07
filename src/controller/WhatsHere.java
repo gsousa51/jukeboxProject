55c76f29598b7e84f67b580ee4c82ff3a2d3583d
Rick Mercer <mercer@cs.arizona.edu>
2016-10-07 10:30:17 -0700

    Initial commit

--- src/controller/WhatsHere.java
+++ src/controller/WhatsHere.java
@@ -3,7 +3,10 @@ package controller;
 public class WhatsHere {
 
   public static void main(String[] args) {
-    System.out.println("This package should contain all types that can be unit tested");
+    System.out.println("This package should contain two GUIs with several controllers."
+        + "One GUI is a prototype (spike) for iteration 1 to allow system testing of the model"
+        + "Another for iteration 2 that is the final version");
+
   }
 
 }
--- src/model/WhatsHere.java
+++ src/model/WhatsHere.java
@@ -3,9 +3,7 @@ package model;
 public class WhatsHere {
 
   public static void main(String[] args) {
-    System.out.println("This package should contain two GUIs with several controllers."
-        + "One GUI is a prototype (spike) for iteration 1 to allow system testing of the model"
-        + "Another for iteration 2 that is the final version");
+    System.out.println("This package should contain all model type taht can be unit tested");
 
   }
 

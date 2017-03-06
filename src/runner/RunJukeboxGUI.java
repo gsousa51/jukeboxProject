cc79654a1c7e448f76ed1eee0fb1969d5bcfb9b6
Stephen Nolan <spn@email.arizona.edu>
2017-03-05 18:33:42 -0700

    Use gridLayout on frame - moving closer to rick's GUI

--- src/runner/RunJukeboxGUI.java
+++ src/runner/RunJukeboxGUI.java
@@ -9,14 +9,19 @@ Assignment: Project 5 - Jukebox
 Due: Friday 10 March 2017 @ 10:00p
 
 
-This class runs the Jukebox GUI for iteration 1
+This class runs the Jukebox GUI for iteration 1 - designed so as to closely 
+model Rick's provided GUI
 
 */
 
 package runner;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 
+import javax.swing.Box;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
@@ -37,6 +42,8 @@ public class RunJukeboxGUI {
         // Frame to hold panels
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Note: 3x1 to match look of Rick's GUI
+        window.setLayout(new GridLayout(3, 1));
 
         // Add the select-songs-with-buttons panel
         window.add(new SongSelectionButtonsPanel());

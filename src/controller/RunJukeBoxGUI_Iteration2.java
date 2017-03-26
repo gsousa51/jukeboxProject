721d01a765a4af10bd5153f429fee0dbe6007a50
Stephen Nolan <spn@email.arizona.edu>
2017-03-26 16:11:12 -0700

    fully working GUI, needs cosmetic finalizations, optionally have "plays today" column update in real time, or just take it out

--- src/controller/RunJukeBoxGUI_Iteration2.java
+++ src/controller/RunJukeBoxGUI_Iteration2.java
@@ -58,255 +58,267 @@ import view.SongSelectionJTablePanel;
 // This Jukebox type extends Jframe
 public class RunJukeBoxGUI_Iteration2 extends JFrame {
 
-	private Song songChosen;
-	private Jukebox juke;
+    private Song songChosen;
+    private Jukebox juke;
 
     // to have access from inside of the listeners
     private SongSelectionJTablePanel songSelectionPanel;
 
-	// main method to run everything
-	public static void main(String[] args) {
-		RunJukeBoxGUI_Iteration2 view = new RunJukeBoxGUI_Iteration2();
+    // main method to run everything
+    public static void main(String[] args) {
+        RunJukeBoxGUI_Iteration2 view = new RunJukeBoxGUI_Iteration2();
 
-	} // method main
+    } // method main
 
-	public RunJukeBoxGUI_Iteration2() {
+    public RunJukeBoxGUI_Iteration2() {
 
         // set nimbus for a nicer experience
-		attemptToSetNimbusLookAndFeel();
+        attemptToSetNimbusLookAndFeel();
 
         // ask if the user wants to start from previously saved state
-		promptUser();
+        promptUser();
 
         // run the actual GUI
-		setUpGUI();
-	}
+        setUpGUI();
+    }
 
-	private void setUpGUI() {
+    private void setUpGUI() {
 
         // HOISTED to pre-GUI code
-		// Set up nimbus to match Rick's GUI
-		//attemptToSetNimbusLookAndFeel();
+        // Set up nimbus to match Rick's GUI
+        //attemptToSetNimbusLookAndFeel();
 
-		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// Note: 3x1 to match look of Rick's GUI
-		this.setLayout(new GridLayout(3, 2));
-		//this.setPreferredSize(new Dimension(750, 750));
-		// this.getContentPane().setBackground(Color.cyan);
-		// this.setBackground(Color.CYAN);
-		// JPanel newFrame = new JPanel();
-		// newFrame.setBackground(Color.CYAN);
-		// newFrame.setSize(new Dimension(750,750));
-		// newFrame.setBackground(Color.CYAN);
-		// this.add(newFrame);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // Note: 3x1 to match look of Rick's GUI
+        this.setLayout(new GridLayout(3, 2));
+        //this.setPreferredSize(new Dimension(750, 750));
+        // this.getContentPane().setBackground(Color.cyan);
+        // this.setBackground(Color.CYAN);
+        // JPanel newFrame = new JPanel();
+        // newFrame.setBackground(Color.CYAN);
+        // newFrame.setSize(new Dimension(750,750));
+        // newFrame.setBackground(Color.CYAN);
+        // this.add(newFrame);
 
-		/*
-		 * NOTE: IF YOU WANT TO TEST THE FUNCTIONALITY OF THIS JFRAME JUST
-		 * UNCOMMENT THE CODE BENEATH THIS NOTE. ALSO, UNCOMMENT THE ADD
-		 * OBSERVER CODE FOR THE SONGSELECTIONPANEL
-		 */
-		// Add the select-songs-with-buttons panel
-		// SongSelectionButtonsPanel selectionArea =
-		// new SongSelectionButtonsPanel(juke);
-		// selectionArea.setBounds(300, 0, 200, 150);
-		// this.add(selectionArea);
+        /*
+         * NOTE: IF YOU WANT TO TEST THE FUNCTIONALITY OF THIS JFRAME JUST
+         * UNCOMMENT THE CODE BENEATH THIS NOTE. ALSO, UNCOMMENT THE ADD
+         * OBSERVER CODE FOR THE SONGSELECTIONPANEL
+         */
+        // Add the select-songs-with-buttons panel
+        // SongSelectionButtonsPanel selectionArea =
+        // new SongSelectionButtonsPanel(juke);
+        // selectionArea.setBounds(300, 0, 200, 150);
+        // this.add(selectionArea);
 
-		/*
-		 * NOTE: The code below is just a rough shot of where the JTable might
-		 * go Obviously, modify this JPanel all you want. You're way better at
-		 * making this stuff pretty than I am.
-		 */
+        /*
+         * NOTE: The code below is just a rough shot of where the JTable might
+         * go Obviously, modify this JPanel all you want. You're way better at
+         * making this stuff pretty than I am.
+         */
 
 
 
         // Add JTable-based panel for selecting and requesting songs
         this.songSelectionPanel = new SongSelectionJTablePanel(juke);
-		// songSelectionPanel.setBackground(Color.BLUE);
-		//songSelectionPanel.setSize(300, 500);
-		//songSelectionPanel.setLocation(400, 35);
-		this.add(songSelectionPanel);
+        // songSelectionPanel.setBackground(Color.BLUE);
+        //songSelectionPanel.setSize(300, 500);
+        //songSelectionPanel.setLocation(400, 35);
+        this.add(songSelectionPanel);
 
-		// Add the label above our playlist
-		JLabel playListLabel = new JLabel();
-		playListLabel.setText("Current song playing is on top of list");
-		Font myFont = new Font("Arial", Font.TRUETYPE_FONT, 16);
-		playListLabel.setFont(myFont);
-		//playListLabel.setSize(300, 50);
-		//playListLabel.setLocation(27, 0);
-		this.add(playListLabel);
+        // Add the label above our playlist
+        JLabel playListLabel = new JLabel();
+        playListLabel.setText("Current song playing is on top of list");
+        Font myFont = new Font("Arial", Font.TRUETYPE_FONT, 16);
+        playListLabel.setFont(myFont);
+        //playListLabel.setSize(300, 50);
+        //playListLabel.setLocation(27, 0);
+        this.add(playListLabel);
 
-		// Add the playlist panel to the JFrame
-		PlaylistPanel playlist = new PlaylistPanel(juke.getSongQueue());
-		// Set its placement/size
-		//playlist.setBounds(0, 35, 300, 500);
-		this.add(playlist);
+        // Add the playlist panel to the JFrame
+        PlaylistPanel playlist = new PlaylistPanel(juke.getSongQueue());
+        // Set its placement/size
+        //playlist.setBounds(0, 35, 300, 500);
+        this.add(playlist);
 
-		// Add the login panel
-		LoginPanel loginArea = new LoginPanel(juke);
-		//loginArea.setBounds(0, 550, 300, 150);
-		this.add(loginArea);
+        // Add the login panel
+        LoginPanel loginArea = new LoginPanel(juke);
+        //loginArea.setBounds(0, 550, 300, 150);
+        this.add(loginArea);
 
-		// Add Arrow Button for Adding the Song
-		BasicArrowButton addSongButton = new BasicArrowButton(BasicArrowButton.WEST);
-		addSongButton.addActionListener(new ButtonListener());
-		//addSongButton.setSize(40, 60);
-		//addSongButton.setLocation(325, 225);
-		this.add(addSongButton);
+        // Add Arrow Button for Adding the Song
+        BasicArrowButton addSongButton = new BasicArrowButton(BasicArrowButton.WEST);
+        addSongButton.addActionListener(new ButtonListener());
+        //addSongButton.setSize(40, 60);
+        //addSongButton.setLocation(325, 225);
+        this.add(addSongButton);
 
-		// register panels as Jukebox observers
-		// juke.addObserver(selectionArea);
-		juke.addObserver(loginArea);
-		this.addWindowListener(new ListenForWindowClose());
+        // register panels as Jukebox observers
+        // juke.addObserver(selectionArea);
+        juke.addObserver(loginArea);
+        this.addWindowListener(new ListenForWindowClose());
 
-		// Pop the GUI
-		this.pack();
-		this.setLocationRelativeTo(null);
-		this.setVisible(true);
-	}
+        // Pop the GUI
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+    }
 
-	private void promptUser() {
-		int userInput = JOptionPane.showConfirmDialog(null, "Use default playlist?");
-		if (userInput == JOptionPane.YES_OPTION) {
-			juke = new Jukebox();
-		} else {
-			FileInputStream stream = null;
-			ObjectInputStream input = null;
-			try {
-				stream = new FileInputStream("savedObjects.txt");
-			} catch (FileNotFoundException e) {
-				e.printStackTrace();
-			}
+    private void promptUser() {
+        int userInput = JOptionPane.showConfirmDialog(null, "Use default playlist?");
+        if (userInput == JOptionPane.YES_OPTION) {
+            juke = new Jukebox();
+        } else {
+            FileInputStream stream = null;
+            ObjectInputStream input = null;
+            try {
+                stream = new FileInputStream("savedObjects.txt");
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
 
-			try {
-				input = new ObjectInputStream(stream);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+            try {
+                input = new ObjectInputStream(stream);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
-			try {
-				juke = (Jukebox) input.readObject();
-				// This call gets the playlist to begin playing automatically
-				juke.getSongQueue().userRestartedSavedJukebox();
-				input.close();
-			} catch (ClassNotFoundException e) {
-				e.printStackTrace();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+            try {
+                juke = (Jukebox) input.readObject();
+                // This call gets the playlist to begin playing automatically
+                juke.getSongQueue().userRestartedSavedJukebox();
+                input.close();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	// If the system has the [Nimbus] Look and Feel, turn it on
-	private static void attemptToSetNimbusLookAndFeel() {
+    // If the system has the [Nimbus] Look and Feel, turn it on
+    private static void attemptToSetNimbusLookAndFeel() {
 
-		try {
+        try {
 
-			for (LookAndFeelInfo lookAndFeel : UIManager.getInstalledLookAndFeels()) {
+            for (LookAndFeelInfo lookAndFeel : UIManager.getInstalledLookAndFeels()) {
 
-				if ("Nimbus".equals(lookAndFeel.getName())) {
+                if ("Nimbus".equals(lookAndFeel.getName())) {
 
-					UIManager.setLookAndFeel(lookAndFeel.getClassName());
-					break;
-				}
-			}
-		}
+                    UIManager.setLookAndFeel(lookAndFeel.getClassName());
+                    break;
+                }
+            }
+        }
 
-		// nimbus wasn't found
-		catch (Exception e) {
+        // nimbus wasn't found
+        catch (Exception e) {
 
-			// system default L&F will be used
-		}
-	}
+            // system default L&F will be used
+        }
+    }
 
-	// private button listener for the arrow buttons.
-	private class ButtonListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent arg0) {
-			// nobody is logged in
-			if (juke.getAccountCollection().getCurrUser() == null) {
+    // private button listener for the arrow buttons.
+    private class ButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent arg0) {
+            // nobody is logged in
+            if (juke.getAccountCollection().getCurrUser() == null) {
 
-				JOptionPane.showMessageDialog(null, "User must log in before selecting a song");
-			}
+                JOptionPane.showMessageDialog(null, "User must log in before selecting a song");
+            }
 
-			// someone is logged in, attempt song play
-			else {
-				/* IMPLEMENTATION DESCRIBED ABOVE
-				 * songChosen= highlightedTextOnJTable
-				 * 
-				 */
+            // someone is logged in, attempt song play
+            else {
+                /* IMPLEMENTATION DESCRIBED ABOVE
+                 * songChosen= highlightedTextOnJTable
+                 * 
+                 */
+
+                // capture the requested song
+                int row = songSelectionPanel.getTable().getSelectedRow();
+                Song requestedSong = (Song) juke.getSongCollection()
+                    .getSong((String) songSelectionPanel.getTable().getValueAt(row, 1));
 
                 // TODO delete - debug
-                System.out.println(songSelectionPanel.getTable().getSelectedRow());
-				
-				// if (!juke.validPlay(songChosen)) {
-				// 	// user has already played 3 songs today - can not play any
-				// 	// more
-				// 	if (juke.getAccountCollection().getCurrUser().getSongsPlayedToday() > 2) {
-				// 		JOptionPane.showMessageDialog(null,
-				// 				juke.getAccountCollection().getCurrUser().getName() + " has reached the limit");
-				// 	}
+                //System.out.println(songSelectionPanel.getTable().getValueAt(row, 1));
 
-				// 	// user does not have enough minutes to play the song
-				// 	else if (juke.getAccountCollection().getCurrUser().getTimeLeft() < songChosen.getLength()) {
 
-				// 		JOptionPane.showMessageDialog(null,juke.getAccountCollection().getCurrUser().getName()
-				// 				+ " does not have enough time credit for this song");
-				// 	}
+                // TODO robust error checking - for example if no row is currently 
+                // selected
 
-				// 	// song already played 3 times today, can not be played
-				// 	// again
-				// 	// today
-				// 	else if (!songChosen.canBePlayed()) {
-				// 		JOptionPane.showMessageDialog(null,
-				// 				songChosen.getSongName() + " has been played 3 times today");
-				// 	}
-				// }
 
-				// // user has enough time credit and song is able to be played
-				// // play song
-				// else {
+                if (!juke.validPlay(requestedSong)) {
+                    // user has already played 3 songs today - can not play any
+                    // more
+                    if (juke.getAccountCollection().getCurrUser().getSongsPlayedToday() > 2) {
+                        JOptionPane.showMessageDialog(null,
+                                juke.getAccountCollection().getCurrUser().getName() + " has reached the limit");
+                    }
 
-				// 	juke.songChosen(songChosen);
-				// }
-			}
-		}
+                    // user does not have enough minutes to play the song
+                    else if (juke.getAccountCollection().getCurrUser().getTimeLeft() < requestedSong.getLength()) {
 
-	}
+                        JOptionPane.showMessageDialog(null,juke.getAccountCollection().getCurrUser().getName()
+                                + " does not have enough time credit for this song");
+                    }
 
-	
-	private class ListenForWindowClose extends WindowAdapter {
+                    // song already played 3 times today, can not be played
+                    // again
+                    // today
+                    else if (!requestedSong.canBePlayed()) {
+                        JOptionPane.showMessageDialog(null,
+                                requestedSong.getSongName() + " has been played 3 times today");
+                    }
+                }
 
-		@Override
-		public void windowClosing(WindowEvent e) {
+                // user has enough time credit and song is able to be played
+                // play song
+                else {
 
-			int userInput = JOptionPane.showConfirmDialog(null, "Save this playlist");
-			if (userInput == JOptionPane.YES_OPTION) {
-				FileOutputStream stream = null;
-				ObjectOutputStream output = null;
-				try {
-					stream = new FileOutputStream("savedObjects.txt");
-				} catch (FileNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				try {
-					output = new ObjectOutputStream(stream);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				try {
-					// let the playlist know that we closed the window but saved
-					// the music.
-					juke.getSongQueue().userClosedWindow();
-					output.writeObject(juke);
-					// output.writeObject(queue);
-					// output.writeObject(accounts);
-				} catch (IOException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-			}
-		}
-	}
+                    // Request the song
+                    juke.songChosen(requestedSong);
+
+                }
+            }
+        }
+
+    }
+
+    
+    private class ListenForWindowClose extends WindowAdapter {
+
+        @Override
+        public void windowClosing(WindowEvent e) {
+
+            int userInput = JOptionPane.showConfirmDialog(null, "Save this playlist");
+            if (userInput == JOptionPane.YES_OPTION) {
+                FileOutputStream stream = null;
+                ObjectOutputStream output = null;
+                try {
+                    stream = new FileOutputStream("savedObjects.txt");
+                } catch (FileNotFoundException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                try {
+                    output = new ObjectOutputStream(stream);
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+                try {
+                    // let the playlist know that we closed the window but saved
+                    // the music.
+                    juke.getSongQueue().userClosedWindow();
+                    output.writeObject(juke);
+                    // output.writeObject(queue);
+                    // output.writeObject(accounts);
+                } catch (IOException e1) {
+                    // TODO Auto-generated catch block
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
 }
